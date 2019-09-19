930ab2bf76e28a80c7311e6660f1d0130381dd3a
Pat Wall <patgwall@iu.edu>
2019-09-19 11:46:59 -0400

    Added some data extraction functions, changed names, exception handling

--- pyMCDS.py
+++ pyMCDS.py
@@ -1,4 +1,4 @@
-import xml.etree.ElementTree as TE
+import xml.etree.ElementTree as ET
 import numpy as np
 import pandas as pd
 import scipy.io as sio
@@ -28,18 +28,69 @@ class pyMCDS:
     def __init__(self, xml_file, output_path='.'):
         self.data = self._read_xml(xml_file, output_path)
 
-    def get_cells_df(self):
+    ## METADATA RELATED FUNCTIONS
+
+    def get_time(self):
+        return self.data['metadata']['current_time']
+
+    ## MESH RELATED FUNCTIONS
+
+    def get_mesh(self, flat=False):
         '''
-        Builds DataFrame from data['discrete_cells']
+        Return a meshgrid of the computational domain. Can return either full
+        3D or a 2D plane for contour plots.
+
+        Parameters
+        ----------
+        flat : bool
+            If flat is set to true, we return only the x and y meshgrid.
+            Otherwise we return x, y, and z
 
         Returns
         -------
-        cells_df : pd.Dataframe [n_cells, n_variables]
-            Dataframe containing the cell data for all cells at this time step
-        '''
-        cells_df = pd.DataFrame(self.data['discrete_cells'])
-        return cells_df
+        xx, yy, zz : three arrays, each [nx_voxel, ny_voxel, nz_voxel]
+            Each array contains coordinates to describe the x, y, and z values 
+            of every voxel center in the computational domain.
         
+        if flat == True
+        xx, yy : two arrays, each [nx_voxel, ny_voxel]
+            Each array contains coordinates to describe the x and y values of
+            flattened "voxels" on a plane
+        '''
+        if flat == True:
+            xx = self.data['mesh']['x_coordinates'][:, :, 0]
+            yy = self.data['mesh']['y_coordinates'][:, :, 0]
+
+            return xx, yy
+
+        # if we dont want a plane just return appropriate values
+        else:
+            xx = self.data['mesh']['x_coordinates']
+            yy = self.data['mesh']['y_coordinates']
+            zz = self.data['mesh']['z_coordinates']
+
+            return xx, yy, zz
+
+    def get_2D_mesh(self):
+        '''
+        This functional returns the x, y meshgrid as two numpy arrays. It is 
+        identical to get_mesh with the option flat=True
+
+        Returns
+        -------
+        xx : ndarray [nx_voxels, ny_voxels]
+            x-coordinates for every voxel center in the computational domain
+        
+        yy : ndarray [nx_voxels, ny_voxels]
+            y-coordinates for every voxel center in the computational domain
+        '''
+        xx = self.data['mesh']['x_coordinates'][:, :, 0]
+        yy = self.data['mesh']['y_coordinates'][:, :, 0]
+
+        return xx, yy
+
+    ## MICROENVIRONMENT RELATED FUNCTIONS
+
     def get_menv_species_list(self):
         '''
         Returns list of chemical species in microenvironment
@@ -52,26 +103,56 @@ class pyMCDS:
         species_list = []
         for name in self.data['continuum_variables']:
             species_list.append(name)
-        
+
         return species_list
-
-    def get_concentrations(self, species):
+    
+    def get_concentrations(self, species_name, z_slice=None):
         '''
-        Returns the concentration arrays for the specified chemical species
-        in the microenvironment.
+        Returns the concentration array for the specified chemical species
+        in the microenvironment. Can return either the whole 3D picture, or
+        a 2D plane of concentrations
 
+        Parameters
+        ----------
+        species_name : string
+            Name of the chemical species to get concentrations for
+        
+        z_slice : float
+            z-axis position to use as plane for 2D output
         Returns
         -------
-        conc_arr : array-like (np.float) [x_voxels, y_voxels, z_voxels]
+        conc_arr : ndarray (np.float) [x_voxels, y_voxels, z_voxels]
             Contains the concentration of the specified chemical in each voxel.
             The array spatially maps to a meshgrid of the voxel centers.
         '''
-        conc_arr = self.data['continuum_variables'][species]['data']
+        if z_slice is not None:
+            # check to see that z_slice is a valid plane
+            zz = self.data['mesh']['z_coordinates']
+            assert z_slice in zz, 'Specified z_slice {} not in z_coordinates'.format(z_slice)
+
+            # do the processing if its ok
+            mask = zz == z_slice
+            full_conc = self.data['continuum_variables'][species_name]['data']
+            conc_arr = full_conc[mask].reshape((zz.shape[0], zz.shape[1]))
+        # its much simpler if we just want the whole thing
+        else:
+            conc_arr = self.data['continuum_variables'][species_name]['data']
+
         return conc_arr
 
-    def get_time(self):
-        return self.data['metadata']['current_time']
+    ## CELL RELATED FUNCTIONS
 
+    def get_cell_df(self):
+        '''
+        Builds DataFrame from data['discrete_cells']
+
+        Returns
+        -------
+        cells_df : pd.Dataframe [n_cells, n_variables]
+            Dataframe containing the cell data for all cells at this time step
+        '''
+        cells_df = pd.DataFrame(self.data['discrete_cells'])
+        return cells_df
         
     def _read_xml(self, xml_file, output_path='.'):
         '''
@@ -80,11 +161,7 @@ class pyMCDS:
 
         output_path = Path(output_path)
         xml_file = output_path / xml_file
-        try:
-            tree = TE.parse(xml_file)
-        except:
-            print('Data File:', xml_file, 'not found!')
-            exit(0)
+        tree = ET.parse(xml_file)
 
         root = tree.getroot()
         MCDS = {}
@@ -141,10 +218,9 @@ class pyMCDS:
         voxel_path = output_path / voxel_file
         try:
             initial_mesh = sio.loadmat(voxel_path)['mesh']
-        except:
-            print('Data file', voxel_path, 'missing!')
-            print('Referenced in', xml_file)
-            exit(0)
+        except IOError as e:
+            print('Bad filename reference in {:s}'.format(xml_file))
+            raise
 
         # center of voxel specified by first three rows [ x, y, z ]
         # volume specified by fourth row
@@ -168,10 +244,9 @@ class pyMCDS:
         me_path = output_path / me_file
         try:
             me_data = sio.loadmat(me_path)['multiscale_microenvironment']
-        except:
-            print('Data file', me_path, 'missing!')
-            print('Referenced in', xml_file)
-            exit(0)
+        except IOError as e:
+            print('Bad filename reference in {:s}'.format(xml_file))
+            raise
 
         var_children = variables_node.findall('variable')
 
@@ -234,10 +309,9 @@ class pyMCDS:
         cell_path = output_path / cell_file
         try:
             cell_data = sio.loadmat(cell_path)['cells']
-        except:
-            print('Data file', cell_path, 'missing!')
-            print('Referenced in', xml_file)
-            exit(0)
+        except IOError as e:
+            print('Bad filename reference in {:s}'.format(xml_file))
+            raise
 
         for col in range(len(data_labels)):
             MCDS['discrete_cells'][data_labels[col]] = cell_data[col, :]
