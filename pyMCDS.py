201b5d50f71263692fcdfd27564825e01ce0064b
Pat Wall <patgwall@iu.edu>
2019-09-25 18:45:50 -0400

    Single voxel functionality

--- pyMCDS.py
+++ pyMCDS.py
@@ -3,29 +3,30 @@ import numpy as np
 import pandas as pd
 import scipy.io as sio
 import sys
+import warnings
 from pathlib import Path
 
 class pyMCDS:
-    '''
+    """
     This class contains a dictionary of dictionaries that contains all of the 
     output from a single time step of a PhysiCell Model. This class assumes that
     all output files are stored in the same directory. Data is loaded by reading
     the .xml file for a particular timestep.
-
+    
     Parameters
     ----------
-    xml_name : string
+    xml_name: str
         String containing the name of the xml file without the path
-    output_path : string (default: '.')
+    output_path: str, optional
         String containing the path (relative or absolute) to the directory
-        where PhysiCell output files are stored
-    
+        where PhysiCell output files are stored (default= ".")
+
     Attributes
     ----------
-    data : dictionary
+    data : dict
         Hierarchical container for all of the data retrieved by parsing the xml
         file and the files referenced therein.
-    '''
+    """
     def __init__(self, xml_file, output_path='.'):
         self.data = self._read_xml(xml_file, output_path)
 
@@ -37,7 +38,7 @@ class pyMCDS:
     ## MESH RELATED FUNCTIONS
 
     def get_mesh(self, flat=False):
-        '''
+        """
         Return a meshgrid of the computational domain. Can return either full
         3D or a 2D plane for contour plots.
 
@@ -49,20 +50,15 @@ class pyMCDS:
 
         Returns
         -------
-        xx, yy, zz : three arrays, each [nx_voxel, ny_voxel, nz_voxel]
-            Each array contains coordinates to describe the x, y, and z values 
-            of every voxel center in the computational domain.
-        
-        if flat == True
-        xx, yy : two arrays, each [nx_voxel, ny_voxel]
-            Each array contains coordinates to describe the x and y values of
-            flattened "voxels" on a plane
-        '''
+        splitting : list length=2 if flat=True, else length=3
+            Contains arrays of voxel center coordinates as meshgrid with shape 
+            [nx_voxel, ny_voxel, nz_voxel] or [nx_voxel, ny_voxel] if flat=True.
+        """
         if flat == True:
             xx = self.data['mesh']['x_coordinates'][:, :, 0]
             yy = self.data['mesh']['y_coordinates'][:, :, 0]
 
-            return xx, yy
+            return [xx, yy]
 
         # if we dont want a plane just return appropriate values
         else:
@@ -70,60 +66,47 @@ class pyMCDS:
             yy = self.data['mesh']['y_coordinates']
             zz = self.data['mesh']['z_coordinates']
 
-            return xx, yy, zz
+            return [xx, yy, zz]
 
     def get_2D_mesh(self):
-        '''
-        This functional returns the x, y meshgrid as two numpy arrays. It is 
+        """
+        This function returns the x, y meshgrid as two numpy arrays. It is 
         identical to get_mesh with the option flat=True
 
         Returns
         -------
-        xx : ndarray [nx_voxels, ny_voxels]
-            x-coordinates for every voxel center in the computational domain
-        
-        yy : ndarray [nx_voxels, ny_voxels]
-            y-coordinates for every voxel center in the computational domain
-        '''
+        splitting : list length=2
+            Contains arrays of voxel center coordinates in x and y dimensions 
+            as meshgrid with shape [nx_voxel, ny_voxel]
+        """
         xx = self.data['mesh']['x_coordinates'][:, :, 0]
         yy = self.data['mesh']['y_coordinates'][:, :, 0]
 
-        return xx, yy
+        return [xx, yy]
 
-    def get_linear_coordinates(self):
-        '''
-        Return all of the unique coordinates of voxel centers for x-, y-, and,
-        z-directions in the mesh.
-
-        Returns
-        -------
-        X : ndarray [nx_voxel]
-            ordered x values of voxel centers
-        Y : ndarray [ny_voxel]
-            ordered y values of voxel centers
-        Z : ndarray [nz_voxels]
-            ordered z values of voxel centers
-        '''
-        xx, yy, zz = self.get_mesh()
-
-        X = np.unique(xx)
-        Y = np.unique(yy)
-        Z = np.unique(zz)
-
-        return X, Y, Z
+    def get_linear_voxels(self):
+        """
+        Helper function to quickly grab voxel centers array stored linearly as
+        opposed to meshgrid-style.
+        """
+        return self.data['mesh']['voxels']['centers']
 
     def get_mesh_spacing(self):
-        '''
+        """
         Returns the space in between voxel centers for the mesh in terms of the
-        mesh's spatial units.
+        mesh's spatial units. Assumes that voxel centers fall on integer values.
 
         Returns
         -------
         dx : float
             Distance between voxel centers in the same units as the other 
             spatial measurements
-        '''
-        X, Y, Z = self.get_linear_coordinates()
+        """
+        centers = self.get_linear_voxels()
+        X = np.unique(centers[0, :])
+        Y = np.unique(centers[1, :])
+        Z = np.unique(centers[2, :])
+
         dx = (X.max() - X.min()) / X.shape[0]
         dy = (Y.max() - Y.min()) / Y.shape[0]
         dz = (Z.max() - Z.min()) / Z.shape[0]
@@ -134,17 +117,70 @@ class pyMCDS:
         
         return round(dx)
 
+    def get_containing_voxel_ijk(self, x, y, z):
+        """
+        Internal function to get the meshgrid indices for the center of a voxel
+        that contains the given position. 
+        
+        Note that pyMCDS stores meshgrids as 'cartesian' 
+        (indexing='xy' in np.meshgrid) which means that we will have
+        to use these indices as [j, i, k] on the actual meshgrid objects
+
+        Parameters
+        ----------
+        x : float
+            x-coordinate for the position
+        y : float
+            y-coordinate for the position
+        z : float
+            z-coordinate for the position
+
+        Returns
+        -------
+        ijk : list length=3
+            contains the i, j, and k indices for the containing voxel's center
+        """
+        xx, yy, zz = self.get_mesh()
+        ds = self.get_mesh_spacing()
+
+        if x > xx.max():
+            warnings.warn('Position out of bounds: x out of bounds in pyMCDS._get_voxel_idx({0}, {1}, {2}). Setting x = x_max!'.format(x, y, z))
+            x = xx.max()
+        elif x < xx.min():
+            warnings.warn('Position out of bounds: x out of bounds in pyMCDS._get_voxel_idx({0}, {1}, {2}). Setting x = x_min!'.format(x, y, z))
+            x = xx.min()
+        elif y > yy.max():
+            warnings.warn('Position out of bounds: y out of bounds in pyMCDS._get_voxel_idx({0}, {1}, {2}). Setting y = y_max!'.format(x, y, z))
+            y = yy.max()
+        elif y < yy.min():
+            warnings.warn('Position out of bounds: y out of bounds in pyMCDS._get_voxel_idx({0}, {1}, {2}). Setting y = y_min!'.format(x, y, z))
+            y = yy.min()
+        elif z > zz.max():
+            warnings.warn('Position out of bounds: z out of bounds in pyMCDS._get_voxel_idx({0}, {1}, {2}). Setting z = z_max!'.format(x, y, z))
+            z = zz.max()
+        elif z < zz.min():
+            warnings.warn('Position out of bounds: z out of bounds in pyMCDS._get_voxel_idx({0}, {1}, {2}). Setting z = z_min!'.format(x, y, z))
+            z = zz.min()
+        
+        i = np.round((x - xx.min()) / ds)
+        j = np.round((y - yy.min()) / ds)
+        k = np.round((z - zz.min()) / ds)
+
+        ii, jj, kk = int(i), int(j), int(k)
+
+        return [ii, jj, kk]
+
     ## MICROENVIRONMENT RELATED FUNCTIONS
 
     def get_substrate_names(self):
-        '''
+        """
         Returns list of chemical species in microenvironment
 
         Returns
         -------
-        species_list : array-like (str) [n_species,]
+        species_list : array (str), shape=[n_species,]
             Contains names of chemical species in microenvironment
-        '''
+        """
         species_list = []
         for name in self.data['continuum_variables']:
             species_list.append(name)
@@ -152,24 +188,25 @@ class pyMCDS:
         return species_list
     
     def get_concentrations(self, species_name, z_slice=None):
-        '''
+        """
         Returns the concentration array for the specified chemical species
         in the microenvironment. Can return either the whole 3D picture, or
-        a 2D plane of concentrations
+        a 2D plane of concentrations.
 
         Parameters
         ----------
-        species_name : string
-            Name of the chemical species to get concentrations for
+        species_name : str
+            Name of the chemical species for which to get concentrations
         
         z_slice : float
-            z-axis position to use as plane for 2D output
+            z-axis position to use as plane for 2D output. This value must match
+            a plane of voxel centers in the z-axis.
         Returns
         -------
-        conc_arr : ndarray (np.float) [x_voxels, y_voxels, z_voxels]
+        conc_arr : array (np.float) shape=[nx_voxels, ny_voxels, nz_voxels]
             Contains the concentration of the specified chemical in each voxel.
             The array spatially maps to a meshgrid of the voxel centers.
-        '''
+        """
         if z_slice is not None:
             # check to see that z_slice is a valid plane
             zz = self.data['mesh']['z_coordinates']
@@ -179,45 +216,110 @@ class pyMCDS:
             mask = zz == z_slice
             full_conc = self.data['continuum_variables'][species_name]['data']
             conc_arr = full_conc[mask].reshape((zz.shape[0], zz.shape[1]))
-        # its much simpler if we just want the whole thing
         else:
             conc_arr = self.data['continuum_variables'][species_name]['data']
 
         return conc_arr
 
+    def get_concentrations_at(self, x, y, z):
+        """
+        Return concentrations of each chemical species inside a particular voxel
+        that contains the point described in the arguments.
+        
+        Parameters
+        ----------
+        x : float
+            x-position for the point of interest
+        y : float
+            y_position for the point of interest
+        z : float
+            z_position for the point of interest
+        
+        Returns
+        -------
+        concs : array, shape=[n_substrates,]
+            array of concentrations in the order given by get_substrate_names()
+        """
+        i, j, k = self.get_containing_voxel_ijk(x, y, z)
+        sub_name_list = self.get_substrate_names()
+        concs = np.zeros(len(sub_name_list))
+
+        for ix in range(len(sub_name_list)):
+            concs[ix] = self.get_concentrations(sub_name_list[ix])[j, i, k]
+        
+        return concs
+
+
     ## CELL RELATED FUNCTIONS
 
     def get_cell_df(self):
-        '''
+        """
         Builds DataFrame from data['discrete_cells']
 
         Returns
         -------
-        cells_df : pd.Dataframe [n_cells, n_variables]
+        cells_df : pd.Dataframe, shape=[n_cells, n_variables]
             Dataframe containing the cell data for all cells at this time step
-        '''
+        """
         cells_df = pd.DataFrame(self.data['discrete_cells'])
         return cells_df
     
     def get_cell_variables(self):
-        '''
-        Returns the names of all of the cell variables tracked in 'diccrete cells'
+        """
+        Returns the names of all of the cell variables tracked in ['discrete cells']
         dictionary
 
         Returns
         -------
-        var_list : list (string) [n_variables]
+        var_list : list, shape=[n_variables]
             Contains the names of the cell variables
-        '''
+        """
         var_list = []
         for name in self.data['discrete_cells']:
             var_list.append(name)
         return var_list
-        
+
+    def get_cell_df_at(self, x, y, z):
+        """
+        Returns a dataframe for cells in the same voxel as the position given by
+        x, y, and z.
+
+        Parameters
+        ----------
+        x : float
+            x-position for the point of interest
+        y : float
+            y_position for the point of interest
+        z : float
+            z_position for the point of interest
+
+        Returns
+        -------
+        vox_df : pd.DataFrame, shape=[n_cell_in_voxel, n_variables]
+            cell dataframe containing only cells in the same voxel as the point 
+            specified by x, y, and z.
+        """
+        ds = self.get_mesh_spacing()
+        xx, yy, zz = self.get_mesh()
+        i, j, k = self.get_containing_voxel_ijk(x, y, z)
+        x_vox = xx[j, i, k]
+        y_vox = yy[j, i, k]
+        z_vox = zz[j, i, k]
+
+        cell_df = self.get_cell_df()
+        inside_voxel = ( (cell_df['position_x'] < x_vox + ds/2.) &
+                         (cell_df['position_x'] > x_vox - ds/2.) &
+                         (cell_df['position_y'] < y_vox + ds/2.) &
+                         (cell_df['position_y'] > y_vox - ds/2.) &
+                         (cell_df['position_z'] < z_vox + ds/2.) &
+                         (cell_df['position_z'] > z_vox - ds/2.) )
+        vox_df = cell_df[inside_voxel]
+        return vox_df
+
     def _read_xml(self, xml_file, output_path='.'):
-        '''
+        """
         Does the actual work of initializing MultiCellDS by parsing the xml
-        '''
+        """
 
         output_path = Path(output_path)
         xml_file = output_path / xml_file
@@ -246,13 +348,6 @@ class pyMCDS:
         mesh_node = me_node.find('mesh')
         MCDS['metadata']['spatial_units'] = mesh_node.get('units')
         MCDS['mesh'] = {}
-
-        # check for cartesian mesh
-        # THIS DOESNT ACTUALLY DO ANYTHING YET
-        cartesian = False
-        mesh_type = mesh_node.get('type')
-        if mesh_type == 'Cartesian':
-            cartesian = True
 
         # while we're at it, find the mesh
         coord_str = mesh_node.find('x_coordinates').text
