72007bc565a780876cc7464df8d4e1129e5860b2
bue <105590-bue@users.noreply@gitlab.com>
2022-10-16 12:31:51 -0400

    @ test_snapshot.py : TestPyMcdsMicroenvTrue implementation beeing continued.

--- test/test_snapshot.py
+++ test/test_snapshot.py
@@ -22,9 +22,9 @@ import pathlib
 import pcDataLoader as pc
 
 # const
-s_path = str(pathlib.Path(pc.__file__).parent.resolve()/'data_snapshot')
-s_file = 'output00003696.xml'
-s_pathfile = f'{s_path}/{s_file}'
+s_path_3d = str(pathlib.Path(pc.__file__).parent.resolve()/'data_snapshot')
+s_file_3d = 'output00003696.xml'
+s_pathfile_3d = f'{s_path_3d}/{s_file_3d}'
 
 # load physicell data shortcut
 class TestPyMcdsShortcut(object):
@@ -32,18 +32,18 @@ class TestPyMcdsShortcut(object):
 
     def test_pyMCDS(self):
         # load physicell data shortcut
-        print(f'process: pc.pyMCDS(xml_file={s_pathfile}) ...')
-        mcds = pc.pyMCDS(xml_file=s_pathfile)
+        print(f'process: pc.pyMCDS(xml_file={s_pathfile_3d}) ...')
+        mcds = pc.pyMCDS(xml_file=s_pathfile_3d)
         assert str(type(mcds)) == "<class 'pcDataLoader.pyMCDS.pyMCDS'>"
 
 # load physicell data with microenvironment
 class TestPyMcdsMicroenvTrue(object):
     ''' test for pc.pyMCDS data loader microenvironment True'''
-    mcds = pc.pyMCDS(xml_file=s_file, output_path=s_path, microenv=True)
+    mcds = pc.pyMCDS(xml_file=s_file_3d, output_path=s_path_3d, microenv=True)
 
     def test_pyMCDS(self, mcds=mcds):
         # load physicell data
-        print(f'process: pc.pyMCDS(xml_file=s_file, output_path=s_path, microenv=True) ...')
+        print(f'process: pc.pyMCDS(xml_file={s_file_3d}, output_path={s_path_3d}, microenv=True) ...')
         assert str(type(mcds)) == "<class 'pcDataLoader.pyMCDS.pyMCDS'>"
 
         # metadata realted functions
@@ -102,30 +102,61 @@ class TestPyMcdsMicroenvTrue(object):
         li_voxel = mcds.get_containing_voxel_ijk(x=0, y=0, z=0)
         assert li_voxel == [37, 37, 37]
 
+    # micro environment related functions
+    def test_mcds_get_substrate_names(self, mcds=mcds):
+        print(f'process: mcds.get_substrate_names() ...')
+        ls_substrate = mcds.get_substrate_names()
+        assert ls_substrate == ['immunostimulatory factor', 'oxygen']
 
-        # micro environment related functions
-    #def test_mcds_get_(self, mcds=mcds):
-    #    mcds.get_substrate_names()
+    def test_mcds_get_concentrations(self, mcds=mcds):
+        print(f'process: mcds.get_concentrations(species_name="oxygen", z_slice=None) ...')
+        ar_conc = mcds.get_concentrations(species_name='oxygen', z_slice=None)
+        assert (str(type(ar_conc)) == "<class 'numpy.ndarray'>") and \
+               (ar_conc.shape == (75, 75, 75))
 
-    #def test_mcds_get_(self, mcds=mcds):
-    #    mcds.get_concentrations(species_name, z_slice=None)
+    def test_mcds_get_concentrations_zslice(self, mcds=mcds):
+        print(f'process: mcds.get_concentrations(species_name="oxygen", z_slice=0) ...')
+        ar_conc = mcds.get_concentrations(species_name='oxygen', z_slice=0)
+        assert (str(type(ar_conc)) == "<class 'numpy.ndarray'>") and \
+               (ar_conc.shape == (75, 75))
 
-    #def test_mcds_get_(self, mcds=mcds):
-    #    mcds.get_concentrations_df(self, z_slice=None)
+    def test_mcds_get_concentrations_df(self, mcds=mcds):
+        print(f'process: mcds.get_concentrations_df(z_slice=None) ...')
+        df_conc = mcds.get_concentrations_df(z_slice=None)
+        assert (str(type(df_conc)) == "<class 'pandas.core.frame.DataFrame'>") and \
+               (df_conc.shape == (421875, 8))
 
-    #def test_mcds_get_(self, mcds=mcds):
-    #    mcds.get_concentrations_at(self, x, y, z=0)
+    def test_mcds_get_concentrations_df_zslice(self, mcds=mcds):
+        print(f'process: mcds.get_concentrations_df(z_slice=0) ...')
+        df_conc = mcds.get_concentrations_df(z_slice=0)
+        assert (str(type(df_conc)) == "<class 'pandas.core.frame.DataFrame'>") and \
+               (df_conc.shape == (5625, 8))
 
+    def test_mcds_get_concentrations_at(self, mcds=mcds):
+        print(f'process: mcds.get_concentrations_at(x=0, y=0, z=0) ...')
+        ar_conc = mcds.get_concentrations_at(x=0, y=0, z=0)
+        assert (str(type(ar_conc)) == "<class 'numpy.ndarray'>") and \
+               (ar_conc.shape == (2,))
 
-        # cell realted functions
-    #def test_mcds_get_(self, mcds=mcds):
-    #    mcds.get_cell_variables()
+    # cell realted functions
+    def test_mcds_get_variables(self, mcds=mcds):
+        print(f'process: mcds.get_cell_variables() ...')
+        ls_variable = mcds.get_cell_variables()
+        assert (str(type(ls_variable)) == "<class 'list'>") and \
+               (len(ls_variable) == 32) and \
+               (ls_variable[0] == 'ID')
 
-    #def test_mcds_get_(self, mcds=mcds):
-    #    mcds.get_cell_df()
+    def test_mcds_get_cell_df(self, mcds=mcds):
+        print(f'process: mcds.get_cell_df() ...')
+        df_cell = mcds.get_cell_df()
+        assert (str(type(df_cell)) == "<class 'pandas.core.frame.DataFrame'>") and \
+               (df_cell.shape == (66978, 37))
 
-    #def test_mcds_get_(self, mcds=mcds):
-    #    mcds.get_cell_df_at()
+    def test_mcds_get_cell_df_at(self, mcds=mcds):
+        print(f'process: mcds.get_cell_df_at(x=0, y=0, z=0) ...')
+        df_cell = mcds.get_cell_df_at(x=0, y=0, z=0)
+        assert (str(type(df_cell)) == "<class 'pandas.core.frame.DataFrame'>") and \
+               (df_cell.shape == (0,37))
 
 
 class TestPyMcdsMicroenvFalse(object):
@@ -133,8 +164,8 @@ class TestPyMcdsMicroenvFalse(object):
 
     def test_pyMCDS(self):
         # load physicell data
-        print(f'process: pc.pyMCDS(xml_file=s_file, output_path=s_path, microenv=False) ...')
-        mcds = pc.pyMCDS(xml_file=s_file, output_path=s_path, microenv=False)
+        print(f'process: pc.pyMCDS(xml_file={s_file_3d}, output_path={s_path_3d}, microenv=False) ...')
+        mcds = pc.pyMCDS(xml_file=s_file_3d, output_path=s_path_3d, microenv=False)
         assert str(type(mcds)) == "<class 'pcDataLoader.pyMCDS.pyMCDS'>"
 
         # metadata realted functions
